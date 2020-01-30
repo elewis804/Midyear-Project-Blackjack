bc6c7a4d41468ea08b0331ca71f0a26cd809819a
Erik <erok84506@gmail.com>
2020-01-30 11:31:35 -0500

    Apparently I did something so...

--- ScreenManager.py
+++ ScreenManager.py
@@ -1,9 +1,5 @@
 from tkinter import *
 from StartScreen import Start
-<<<<<<< HEAD
-
-=======
->>>>>>> 3ddd6df9f9c66eef9807f68b370e3ccec47d240e
 
 class Screen_Manager(object):
     def __init__(self):
@@ -12,7 +8,6 @@ class Screen_Manager(object):
 
     def start_screen(self):
         self.root.title("Start the Game!")
-<<<<<<< HEAD
         self.currentScreen = Start(self.root, self.next_screen())
 
     def next_screen(self):
@@ -25,11 +20,3 @@ def main():
     game.root.mainloop()
 
 main()
-=======
-        self.currentScreen=Start(self.root)
-
-
-
-
-
->>>>>>> 3ddd6df9f9c66eef9807f68b370e3ccec47d240e
