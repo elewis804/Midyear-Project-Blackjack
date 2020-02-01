64a8c8cd20db006a9ee798a08d5e923d4b6f7f8a
Benen <Ranzadet@gmail.com>
2020-01-31 23:22:23 -0500

    minor changes, end of day commit

--- ScreenManager.py
+++ ScreenManager.py
@@ -9,12 +9,18 @@ class Screen_Manager(object):
         self.currentScreen = None
 
     def start_screen(self):
-        self.root.title("Start the Game!")
-        self.currentScreen = Start(self.root, self.next_screen())
+        self.root.title("Main Menu")
+        self.currentScreen = Start(self.root, self.next_screen)
 
     def next_screen(self):
+        self.currentScreen.destroy()
+
         self.currentScreen = ScreenGame(self.root)
 
+    def startup_screengame(self):
+        self.root.title("BlackJack!")
+
+        self.currentScreen = ScreenGame(self.root)
 
 def main():
     game = Screen_Manager()
--- screen_game.py
+++ screen_game.py
@@ -15,6 +15,7 @@ class ScreenGame(Frame):
 
 
         Button(self,text="Hit",command=self.Hit).grid(row=3,column=3,sticky=E)
+        Button(self,text="Stay",command=self.stay).grid(row=3,column=0,sticky=W)
 
 
     def Hit(self):
@@ -23,18 +24,23 @@ class ScreenGame(Frame):
                 self.player1.getRandomCard()
             else:
                 self.turn = 2
-                self.turn_switch("Two")
+                self.turn_switch(self.turn)
         elif self.turn == 2:
             if self.player2.score <= 20:
                 self.player2.getRandomCard()
             else:
                 self.turn = 3
 
+    def stay(self):
+        self.turn += 1
+        if self.turn < 3:
+            self.turn_switch(self.turn)
+
 
     def turn_switch(self,to):
-        '''This method will display a temporary label using time to show the turn has swtiched'''
-        self.switch = Label(self,text=("Turn Switch to player", to),font=18)
-        self.switch.grid(row=0,column=1,sticky=N)
+        '''This method will display a constant label showing whose turn it is'''
+        self.switch = Label(self,text=("Turn: Player" + str(to)),font=18)
+        self.switch.grid(row=5,column=1,sticky=S)
 
 
 
