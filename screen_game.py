dce8f240f89fafc70104da3e79e1d1c878464242
Benen <Ranzadet@gmail.com>
2020-02-01 00:09:43 -0500

    Converted images to png, added background things

--- screen_game.py
+++ screen_game.py
@@ -2,6 +2,9 @@ from tkinter import *
 import time
 from Blackjackinteractions import Player
 
+'''
+TO DO:
+FIX THE IMAGES SO THEY ARE ALL SAME SIZES'''
 
 class ScreenGame(Frame):
     def __init__(self,master):
@@ -17,9 +20,9 @@ class ScreenGame(Frame):
         back = PhotoImage(file="Images-Blackjack/dealertable.png")
         w = Label(self, image=back)
         w.photo = back
-        w.grid(row=0, column=0, rowspan=5,columnspan=5)
+        w.grid(row=0, column=0, rowspan=15,columnspan=15)
 
-        Button(self,text="Hit",command=self.Hit).grid(row=3,column=3,sticky=E)
+        Button(self,text="Hit",command=self.Hit).grid(row=3,column=14,sticky=E)
         Button(self,text="Stay",command=self.stay).grid(row=3,column=0,sticky=W)
 
 
@@ -27,6 +30,11 @@ class ScreenGame(Frame):
         if self.turn == 1:
             if self.player1.score <= 20:
                 self.player1.getRandomCard()
+                card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand)-1].image)
+                x = Label(self, image=card1)
+                x.photo = card1
+                x.grid(row=5,column=3,rowspan=2,columnspan=2)
+
             else:
                 self.turn = 2
                 self.turn_switch(self.turn)
@@ -44,9 +52,6 @@ class ScreenGame(Frame):
 
     def turn_switch(self,to):
         '''This method will display a constant label showing whose turn it is'''
-        self.switch = Label(self,text=("Turn: Player" + str(to)),font=18)
-        self.switch.grid(row=5,column=1,sticky=S)
-
-
-
+        self.switch = Label(self,text=("Turn: Player" + str(to)),font=24)
+        self.switch.grid(row=14,column=7,sticky=S)
 
