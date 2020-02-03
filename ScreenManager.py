26fab04ebd3259593d77d337aee014b8aa76c658
Erik <erok84506@gmail.com>
2020-02-03 08:49:06 -0500

    Got rid of the original start screen function and switched it to the second one

--- ScreenManager.py
+++ ScreenManager.py
@@ -10,16 +10,12 @@ class Screen_Manager(object):
 
     def start_screen(self):
         self.root.title("Main Menu")
-        self.currentScreen = Start(self.root, self.next_screen)
+        self.currentScreen = Start(self.root, self.startup_screengame)
 
-    def next_screen(self):
-        self.currentScreen.destroy()
-
-        self.currentScreen = ScreenGame(self.root)
 
     def startup_screengame(self):
+        self.currentScreen.destroy()
         self.root.title("BlackJack!")
-
         self.currentScreen = ScreenGame(self.root)
 
 def main():
