16cfd76c8556b218d58be0b8a2cea1b6899829e0
Erik <erok84506@gmail.com>
2020-02-07 11:37:51 -0500

    Worked a lot on BlackJackAI

--- BlackjackAI.py
+++ BlackjackAI.py
@@ -8,7 +8,7 @@ class AIGame(Frame):
         self.column = 1
         self.rounds = 0
         self.player1 = Player()
-        self.player2 = Player()
+        self.AIPlayer = Player()
         self.turn = 1
         self.ties = 0
         self.cleared = False
@@ -19,142 +19,85 @@ class AIGame(Frame):
         w = Label(self, image=back)
         w.photo = back
         w.grid(row=0, column=0, rowspan=15,columnspan=15)
-        self.start = Button(self,text="Player Start turn", bg = "indian red",font=("Arial",30,"bold"))
+        self.switch = Label(text="")
+        self.start = Button(self,text="Player Start turn", bg = "indian red",font=("Arial",30,"bold"),command=self.begin)
         self.start.grid(row=5,column=6)
 
 
     def begin(self):
-        if self.rounds > 1:
-            if self.turn == 1:
-                self.switch["text"] = None
-        if self.turn == 1:
-            self.rounds += 1
-            for x in self.player1.hand:
-                self.player1.hand.remove(x)
-            for x in self.player2.hand:
-                self.player2.hand.remove(x)
-            self.player1.score = 0
-            self.player2.score = 0
-        if self.rounds > 1:
-            self.display_current.destroy()
-        if self.turn == 1:
-            self.player1.getRandomCard()
-            self.player1.p_hand.append(0)
-            card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
-            self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
-            self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
-            self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
-            self.column += 1
-            self.player1.getRandomCard()
-            self.player1.p_hand.append(0)
-            card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
-            self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
-            self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
-            self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
-            self.column += 1
-            if self.player1.score == 21:
-                self.x = True
-                self.blackjack = Button(self,text="Player 1 Blackjack",bg="indian red",command=self.stay,font=("Arial",16,"bold"))
-                self.blackjack.grid(row=2,column=4,rowspan=2,columnspan=6)
-        elif self.turn == 2:
-            if not self.cleared:
-                self.clear_board()
-            else:
-                self.cleared = False
-            self.player2.getRandomCard()
-            self.player2.p_hand.append(0)
-            card1 = PhotoImage(file="Images-Blackjack/" + self.player2.hand[len(self.player2.hand) - 1].image)
-            self.player2.p_hand[len(self.player2.p_hand) - 1] = Label(self, image=card1)
-            self.player2.p_hand[len(self.player2.p_hand) - 1].photo = card1
-            self.player2.p_hand[len(self.player2.p_hand) - 1].grid(row=self.row, column=self.column)
-            self.column += 1
-            self.player2.getRandomCard()
-            self.player2.p_hand.append(0)
-            card1 = PhotoImage(file="Images-Blackjack/" + self.player2.hand[len(self.player2.hand) - 1].image)
-            self.player2.p_hand[len(self.player2.p_hand) - 1] = Label(self, image=card1)
-            self.player2.p_hand[len(self.player2.p_hand) - 1].photo = card1
-            self.player2.p_hand[len(self.player2.p_hand) - 1].grid(row=self.row, column=self.column)
-            self.column += 1
-            if self.player2.score == 21:
-                self.x = True
-                self.blackjack = Button(self,text="Player 2 Blackjack",bg="sky blue",command=self.stay,font=("Arial",16,"bold"))
-                self.blackjack.grid(row=2,column=4,rowspan=2,columnspan=6)
         self.start.destroy()
-        if not self.x:
+        self.switch["text"] = None
+        for x in self.player1.hand:
+            self.player1.hand.remove(x)
+        self.player1.score = 0
+        self.player1.getRandomCard()
+        self.player1.p_hand.append(0)
+        card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
+        self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
+        self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
+        self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
+        self.column += 1
+        self.player1.getRandomCard()
+        self.player1.p_hand.append(0)
+        card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
+        self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
+        self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
+        self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
+        self.column += 1
+        if self.player1.score == 21:
+            self.x = True
+            self.blackjack = Button(self,text="Player 1 Blackjack",bg="indian red",command=self.stay,font=("Arial",16,"bold"))
+            self.blackjack.grid(row=2,column=4,rowspan=2,columnspan=6)
+        if self.x == False:
             self.h = Button(self,text="Hit",command=self.Hit)
             self.h.grid(row=2,column=14,sticky=E)
             self.s = Button(self,text="Stay",command=self.stay)
             self.s.grid(row=2,column=0,sticky=W)
-    
-    def turn_switch(self,to):
-        '''This method will display a constant label showing whose turn it is'''
-        if self.turn < 3:
-            self.switch = Label(self,text=("Turn: Player " + str(to)),font=("Times",16,"bold"))
-            self.switch.grid(row=11,column=7,sticky=N)
-        else:
-            self.switch.destroy()
-            self.switch = Label(self,text=("Round Over"),font=("Times",16,"bold"))
-            self.switch.grid(row=11,column=7,sticky=N)
-            self.round_end()
-    
+
     def Hit(self):
-        if self.turn == 1:
+        if self.player1.score <= 20:
             self.player1.getRandomCard()
-            if self.player1.score <= 20:
-                self.player1.p_hand.append(0)
-                if self.column < 4:
-                    card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
-                    self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
-                    self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
-                    self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
-                    self.column += 1
-                else:
-                    self.row += 2
-                    self.column = 1
-                    self.player1.p_hand.append(0)
-                    card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
-                    self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
-                    self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
-                    self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
-            else:
-                self.turn = 2
-                self.player1.bust = True
-                self.clear_board()
-                self.cleared = True
-                self.turn_switch(self.turn)
-                self.start = Button(self, text="Player " + str(self.turn) + " start turn", bg ="sky blue",command=self.begin,
-                                    font=("Arial", 30, "bold"))
-                self.start.grid(row=5, column=6)
-                self.h.destroy()
-                self.s.destroy()
-        elif self.turn == 2:
-            self.player2.getRandomCard()
-            if self.player2.score <= 20:
-                self.player2.p_hand.append(0)
-                if self.column < 4:
-                    card1 = PhotoImage(file="Images-Blackjack/" + self.player2.hand[len(self.player2.hand) - 1].image)
-                    self.player2.p_hand[len(self.player2.p_hand) - 1] = Label(self, image=card1)
-                    self.player2.p_hand[len(self.player2.p_hand) - 1].photo = card1
-                    self.player2.p_hand[len(self.player2.p_hand) - 1].grid(row=self.row, column=self.column)
-                    self.column += 1
-                else:
-                    self.row += 2
-                    self.column = 1
-                    self.player1.p_hand.append(0)
-                    card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
-                    self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
-                    self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
-                    self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
-            else:
-                self.turn = 3
-                self.player2.bust = True
-                self.clear_board()
-                self.cleared = True
-                self.turn_switch(self.turn)
-                self.h.destroy()
-                self.s.destroy()
-                for x in range(len(self.player2.p_hand)):
-                    self.player2.p_hand[x].destroy()
-                for x in self.player2.p_hand:
-                    self.player2.p_hand.remove(x)
+            self.player1.p_hand.append(0)
+            card1 = PhotoImage(file="Images-Blackjack/" + self.player1.hand[len(self.player1.hand) - 1].image)
+            self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
+            self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
+            self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
+        elif self.player1.score > 21:
+            self.player1.bust=TRUE
+            self.AI()
+
+
+
+    def AI(self):
+        self.AIPlayer.getRandomCard()
+        self.AIPlayer.p_hand.append(0)
+        self.AIPlayer.getRandomCard()
+        self.AIPlayer.p_hand.append(0)
+        if self.AIPlayer.score == 21 and self.x==FALSE:
+            print("AI Wins")
+        elif self.AIPlayer.score >= 17:
+            print("Will compare the cards of player and AI")
+        else:
+            self.AIPlayer.getRandomCard()
+            self.AIPlayer.p_hand.append(0)
     
+    
+    def stay(self):
+        self.clear_board()
+        self.AI()
+    
+    def clear_board(self):
+        if not self.cleared:
+            if 0 in self.player1.p_hand:
+                self.player1.p_hand.remove(0)
+            if len(self.player1.p_hand) > 0:
+                for x in range(len(self.player1.p_hand)):
+                    print(self.player1.p_hand[x])
+                    self.player1.p_hand[x].destroy()
+                for x in self.player1.p_hand:
+                    self.player1.p_hand.remove(x)
+            self.cleared = True
+        else:
+            self.cleared = True
+        self.row = 5
+        self.column = 1
