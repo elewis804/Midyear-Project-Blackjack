894ec7fac28c63e4f2b2e7f84c318fcd80f0287c
Hayun <YunJun505@gmail.com>
2020-02-07 11:27:59 -0500

    edit to credit screen

--- creditscreen.py
+++ creditscreen.py
@@ -4,12 +4,14 @@ class Credit(Frame):
     def __init__(self, master):
         super(Credit, self).__init__(master)
         self.grid()
-        self.create_widgets()
         quill = PhotoImage(file="Images-Blackjack/creditscreen.png")
         q = Label(self, image=quill)
         q.photo = quill
         q.grid(row=0, column=0, rowspan=15, columnspan=15)
+        self.create_widgets()
 
 
     def create_widgets(self):
-        Label(text = "Made by Benen Sullivan, Lukas Tegge, Hayun Jung, and Eric Lewis  ", bg = 'gold', font = ("Arial",19, "bold")).grid(row = 1, column = 0, sticky = W )
+        Label(self, text = "Made by Benen Sullivan, Lukas Tegge, Hayun Jung, and Eric Lewis  ", bg = 'gold', font = ("Arial",19, "bold")).grid(row = 1, column = 0, sticky = W )
+        a = Label(self, text = 'Sorry you\'re trapped now, you have to exit to go back', bg = 'gold', font = 'bold')
+        a.grid(row = 2, column = 0, sticky = W)
