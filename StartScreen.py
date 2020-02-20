09f95729ea47714bf3fa8e968dcff10e92f8a5c5
Benen <Ranzadet@gmail.com>
2020-02-20 11:17:08 -0500

    updated start screen

--- StartScreen.py
+++ StartScreen.py
@@ -16,14 +16,14 @@ class Start(Frame):
         w.photo = back
         w.grid(row=0, column=0, rowspan=16, columnspan=16)
         Label(self, text="Welcome to Blackjack!", fg="green",bg = "light goldenrod",font=31).grid(row=0, column=8, sticky=N)
-        self.tpButton = Button(self, text="2 Player", bg="green", font = 24,command=self.startgame2P)
-        self.tpButton.grid(row=3, column=8, sticky=N)
+        self.tpButton = Button(self, text="2 Player", bg="blue", font = 24,command=self.startgame2P)
+        self.tpButton.grid(row=4, column=8, sticky=N)
         self.AIButton = Button(self, text="1 Player", bg="green", font=24, command=self.startgame1P)
-        self.AIButton.grid(row=4, column=8, sticky=N)
-        self.multi = Button(self,text="4 PLayer",bg="green",font=24,command=self.startgame4p)
-        self.multi.grid(row=4,column=8,sticky=N)
+        self.AIButton.grid(row=3, column=8, sticky=N)
+        self.multi = Button(self,text="4 Player",bg="orange",font=24,command=self.startgame4p)
+        self.multi.grid(row=5,column=8,sticky=N)
         self.creditButton = Button(self, text="Credits", bg="red", font=24, command=self.credits)
-        self.creditButton.grid(row=5, column=8, sticky=N)
+        self.creditButton.grid(row=6, column=8, sticky=N)
     
     def startgame2P(self):
         self.close_file1()
