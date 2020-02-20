672f139068ba4056f26c650cf65d1ee4f382f373
lukasgrat <lukasgrat@gmail.com>
2020-02-20 10:47:46 -0500

    Busting and winning/losing is a thing now.Also some of the text boxes now have color

--- BlackjackAI.py
+++ BlackjackAI.py
@@ -55,6 +55,9 @@ class AIGame(Frame):
             self.s.grid(row=2,column=0,sticky=W)
 
     def Hit(self):
+        if self.column >=4:
+            self.row += 1
+            self.column = 1
         if self.player1.score <= 20:
             self.player1.getRandomCard()
             self.player1.p_hand.append(0)
@@ -62,9 +65,19 @@ class AIGame(Frame):
             self.player1.p_hand[len(self.player1.p_hand) - 1] = Label(self, image=card1)
             self.player1.p_hand[len(self.player1.p_hand) - 1].photo = card1
             self.player1.p_hand[len(self.player1.p_hand) - 1].grid(row=self.row, column=self.column)
+            if self.player1.score > 21:
+                self.player1.bust=TRUE
+                self.h.destroy()
+                self.s.destroy()
+                self.clear_board()
+                self.AI()
         elif self.player1.score > 21:
-            self.player1.bust=TRUE
+            self.player1.bust = TRUE
+            self.h.destroy()
+            self.s.destroy()
+            self.clear_board()
             self.AI()
+
 
 
 
@@ -73,13 +86,15 @@ class AIGame(Frame):
         self.AIPlayer.p_hand.append(0)
         self.AIPlayer.getRandomCard()
         self.AIPlayer.p_hand.append(0)
-        if self.AIPlayer.score == 21 and self.x==FALSE:
-            print("AI Wins")
-        elif self.AIPlayer.score >= 17:
-            print("Will compare the cards of player and AI")
-        else:
-            self.AIPlayer.getRandomCard()
-            self.AIPlayer.p_hand.append(0)
+        while True:
+            if self.AIPlayer.score >= 21:
+                self.AIPlayer.bust = True
+                self.round_end()
+            elif self.AIPlayer.score >= 17:
+                self.round_end()
+            else:
+                self.AIPlayer.getRandomCard()
+                self.AIPlayer.p_hand.append(0)
     
     
     def stay(self):
@@ -101,3 +116,54 @@ class AIGame(Frame):
             self.cleared = True
         self.row = 5
         self.column = 1
+
+    def score_display(self):
+        self.p1_wins = Label(self, text="Player 1 wins: " + str(self.player1.win), bg="indian red",
+                             font=("Times", 14))
+        self.p1_wins.grid(row=0, column=5, sticky=N)
+        self.p2_wins = Label(self, text="Player 2 wins:" + str(self.AIPlayer.win), bg="sky blue", font=("Times", 14))
+        self.p2_wins.grid(row=0, column=8, sticky=N)
+    def round_end(self):
+        if self.player1.bust:
+            if self.AIPlayer.bust:
+                self.display_current = Label(self,text="Tie Round",bg="orchid2",font =14)
+                self.display_current.grid(row=13,column=7,sticky=S)
+                self.ties += 1
+            else:
+                self.display_current = Label(self,text="Player 2 Wins Round",bg="indian red")
+                self.display_current.grid(row=13,column=7,sticky=S)
+                if self.rounds > self.AIPlayer.win + self.player1.win - self.ties:
+                    self.AIPlayer.win += 1
+        elif self.AIPlayer.bust:
+            self.display_current = Label(self,text="Player 1 Wins Round",bg="indian red")
+            self.display_current.grid(row=13,column=7,sticky=S)
+            if self.rounds > self.player1.win + self.AIPlayer.win - self.ties:
+                self.player1.win += 1
+        else:
+            if self.player1.score > self.AIPlayer.score:
+                self.display_current = Label(self,text="Player 1 Wins Round",bg="indian red")
+                self.display_current.grid(row=13,column=7,sticky=S)
+                if self.rounds > self.player1.win + self.AIPlayer.win - self.ties:
+                    self.player1.win += 1
+            elif self.AIPlayer.score > self.player1.score:
+                self.display_current = Label(self,text="Player 2 Wins Round",bg="sky blue")
+                self.display_current.grid(row=13,column=7,sticky=S)
+                if self.rounds > self.player1.win + self.AIPlayer.win - self.ties:
+                    self.AIPlayer.win += 1
+            else:
+                self.display_current = Label(self,text="Tie Round",bg ="orchid2")
+                self.display_current.grid(row=13,column=7,sticky=S)
+                self.ties += 1
+        self.score_display()
+        self.switch["text"] = None
+        self.turn = 1
+        self.cleared = False
+        self.x = False
+        self.player1.bust = False
+        self.AIPlayer.bust = False
+        self.end = True
+        self.player1.score = 0
+        self.AIPlayer.score = 0
+        self.start = Button(self, text="Player " + str(self.turn)+" start turn",bg="indian red",command=self.begin,
+                            font=("Arial", 30, "bold"))
+        self.start.grid(row=5, column=6)
